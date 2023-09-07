062255055f0403b54e3d6b880c6dcff3e2c9acd8
codewithastro <amoriamori3016@gmail.com>
2023-09-07 21:47:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//this project doesn't contain any real JS scripts TRUST ME :)
+console.log("Why are you here?")
+console.warn("CLOSE IT PLZ :)")
+
+//Last Script Trust Me
+document.getElementById("what").addEventListener("click", function(){
+   alert("Congratulation You Waste Your Time For This Easter Egg :)")
+})
